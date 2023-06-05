9919cf55ca9b09a829b5b6d45f5c8ee485b6ef10
Kyungwook <ruddnrdl@kaist.ac.kr>
2023-06-05 15:28:29 +0900

    formatted sample.py

--- sample/sample.py
+++ sample/sample.py
@@ -1,9 +1,10 @@
 def two_sum(a, b):
-    c=a
-    d=40
-    a=b
-    b=c
+    c = a
+    d = 40
+    a = b
+    b = c
     return b + a
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     two_sum(4, 2)
