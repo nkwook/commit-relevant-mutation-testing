27153cde176dbe2ac689d999c5488b181d9ffc39
Kyungwook <ruddnrdl@kaist.ac.kr>
2023-06-05 20:26:49 +0900

    remove test 3

--- approximate/post_commit.py.py
+++ approximate/post_commit.py.py
@@ -6,10 +6,8 @@ def func(x: List[int], y: List[int]) -> int:
     R = 2
     if x[R] > y[R]:
         vR = 1
-    elif x[R] == y[R]:
-        L = 0
-        if x[L] > y[L]:
-            vL = 1
-
+    
     if x[0] > y[2]:
         return -1
+
+    return vL + vR
