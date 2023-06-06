5774b85dc28e4f659a9838d20e9052fc88f48fbf
juckchang <c2w2m2@kaist.ac.kr>
2023-06-06 20:45:01 +0900

    case2

--- sample/case2/case2.py
+++ sample/case2/case2.py
@@ -2,7 +2,7 @@ import time
 
 def equation_fibonacci(n):
     root_5 = 5 ** (1/2)
-    return (1 / root_5) * ( ( (1 + root_5) / 2)**n - ( (1 - root_5) / 2)**n )
+    return (1 / root_5) * ( ( (1 + root_5) / 2)**n + ( (1 - root_5) / 2)**n )
 
 if __name__ == '__main__':
     start_time = time.time()
