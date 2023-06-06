aa6c6d03e8a86d2b3df8c8185c3a7e91f5ffdfd0
Kyungwook <ruddnrdl@kaist.ac.kr>
2023-06-07 01:37:29 +0900

    commit for case9

--- samples/case9/case9.py
+++ samples/case9/case9.py
@@ -1,17 +1,18 @@
 def foo1(a, b):
     if a < b:
         return a + b
-    if(b > 10):#
-        return 10
+    elif(b > 10):#
+        return 100
     else:
         return a - b
 
 def foo2(a, b):
     if(a > 10):#
         return a
-    if a < b:
+    elif a < b:
         return a * b
     elif a == b:
+        c = b + 1
         return b * 3
     else:
         return -2
@@ -22,11 +23,17 @@ def foo3(a, b):
     if a >= b:
         return foo1(a, b)
     else:
+        if a < 0:
+            return foo4(a)
         return foo2(a, b)
 
 def foo4(a):
     x = -a
+    if x == 0:
+        return 0
     return str(34) + ' + ' + str(x)
 
 def foo5(a):#
+    if a < 0:
+        return -a
     return a * 42
