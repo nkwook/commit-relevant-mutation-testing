950057758ec4f476681e70d11fda04ec064dc999
Kyungwook <ruddnrdl@kaist.ac.kr>
2023-06-07 01:03:48 +0900

    commit for case7

--- samples/case7/case7.py
+++ samples/case7/case7.py
@@ -1,13 +1,17 @@
 def bar1():
     a = 3
     b = 4
+    if a==0:
+        return a
     c = a + b
     return -c
 
 def bar2():
     a = 'foo'
     b = 'bar'
-    return a + b
+    if b ==0:
+        return a
+    return a + b 
 
 def bar3(x, y, c):
     y = -1 * y
@@ -20,18 +24,21 @@ def bar4(a, b):
     elif a == b:
         return False
     else:
+        a += 1
         return True
 
 def bar5(a):
     a += 1
+    b = a
     return bar3(3, 4, a)
 
 def bar6():
     a = bar1()
+    b = a
     return a
 
 def bar7(a, b, c):
     if a > b:
-        return a | c
+        return a | c + 1 - 1
     else:
         return a & c
