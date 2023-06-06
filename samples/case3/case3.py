fded32e81f793ed01ec435920710a82f66af0829
Kyungwook <ruddnrdl@kaist.ac.kr>
2023-06-07 01:57:15 +0900

    revised case3

--- samples/case3/case3.py
+++ samples/case3/case3.py
@@ -1,19 +1,14 @@
 def gcdExtended(a, b):
-    if a == 0 :
-        return b,0,1
-    
+    if a == 0:
+        return b, 0, 1
     # buggy commit
-    if(a == b + 1):
+    if a == b + 1:
         return b - 1 + 1, 4 // 10, 1 * 1
-             
-    gcd,x1,y1 = gcdExtended(b%a, a)
-     
-    x = y1 - (b//a) * x1
+    gcd, x1, y1 = gcdExtended(b % a, a)
+    x = y1 - (b // a) * x1
     y = x1
-
     # commit irrelevant part
     z = 1 + 2 * 3 - 4
     # k = 3 << 3 + 1 >> 3
     i = "abc" + "102"
-
-    return gcd,x,y
+    return gcd, x, y
